ca5a498f99b460fcb469a01ebd74bcb9245b7658
Sergey-Pavlov <50874253+Sergey-Pavlov@users.noreply.github.com>
2022-11-21 22:13:37 +0300

    DL_POLY CONFIG writer was intoduces

--- mdsolv/io_data/dl_poly.py
+++ mdsolv/io_data/dl_poly.py
@@ -3,13 +3,16 @@ from mdsolv.core.structure import Trajectory
 
 class DL_POLY():
 
-    def __init__(self, system, coordinates):
+    def __init__(self, system):
         self.system = system
 
     def write_CONFIG(self, filename):
         with open(filename, 'w') as f:
             f.write('DL_POLY CONFIG File' + '\n' +
                           '         0         3\n' +
-                          "%20f%20f%20f" % (self.system.box[0]) + '\n' +
-                          "%20f%20f%20f" % (self.system.box[1]) + '\n' +
-                          "%20f%20f%20f" % (self.system.box[2]) + '\n')
+                          "%20f%20f%20f" % tuple(self.system.box[0]) + '\n' +
+                          "%20f%20f%20f" % tuple(self.system.box[1]) + '\n' +
+                          "%20f%20f%20f" % tuple(self.system.box[2]) + '\n')
+            for i in range(len(self.system.atomlist)):
+                f.write("%3s%15s%10s" % (self.system.atomlist[i], i+1, 0) + '\n')
+                f.write("%20f%20f%20f" % tuple(self.system.coordinates[i]) + '\n')
