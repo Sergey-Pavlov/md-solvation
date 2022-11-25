8288bea3ef914a353c9c98d2a3db6ad1f5cbe676
Sergey-Pavlov <50874253+Sergey-Pavlov@users.noreply.github.com>
2022-11-25 15:34:42 +0300

    calculate_orientational_order_1d method was introduced

--- mdsolv/analysis/interface_structure.py
+++ mdsolv/analysis/interface_structure.py
@@ -101,10 +101,10 @@ class Interface_Analyser:
                                       weights=np.tile(charges_flatten, steps))
         volume = box[0] * (x_edges[1] - x_edges[0]) * (y_edges[1] - y_edges[0])
         volume_z = box[0] * box[1] * (bins_z[1] - bins_z[0])
-        Da = 1.66053906660e-24  # Da in gramms
+        E_to_Coulomb = 1.60217663e-19  # Elemetrary charge to Coulomb
         nm_to_cm = 1e-7
-        hist = hist * Da / volume / steps / nm_to_cm ** 3
-        hist_z = hist_z * Da / volume_z / steps / nm_to_cm ** 3
+        hist = hist * E_to_Coulomb / volume / steps / nm_to_cm ** 3
+        hist_z = hist_z * E_to_Coulomb / volume_z / steps / nm_to_cm ** 3
         return hist, x_edges, y_edges, hist_z, bins_z
 
     def calculate_local_density_1d(self, traj_path, dx):
@@ -168,5 +168,51 @@ class Interface_Analyser:
         hist_z = hist_z * Da / volume_z / steps / nm_to_cm ** 3
         return hist_z, bins_z
 
-    def calculate_orientational_order_1d(self, traj_path, dx, mol_type=None):
-        pass
+    def calculate_orientational_order_1d(self, traj_path, dx, mol_type, atomtypes, three_atom_plane=True):
+        from string import digits
+        import pytrr
+        traj_data = []
+        cos_data = []
+        remove_digits = str.maketrans('', '', digits)
+        mol_names = [s.translate(remove_digits) for s in list(self.system.keys())]
+        mol_names = np.array(mol_names)
+        n_atoms_per_mol = np.array(list(map(len, list(self.system.values()))))
+        mollist = np.repeat(mol_names, n_atoms_per_mol)
+        system_flatten = list(itertools.chain(*list(self.system.values())))
+        atomlist = np.array(system_flatten)
+        mask_mol = np.array((mollist == mol_type))
+        if three_atom_plane:
+            mask_atom1 = np.array((atomlist == atomtypes[0])) * mask_mol
+            mask_atom2 = np.array((atomlist == atomtypes[1])) * mask_mol
+            mask_atom3 = np.array((atomlist == atomtypes[2])) * mask_mol
+        else:
+            raise ValueError("Another options are not ready yet")
+        with pytrr.GroTrrReader(traj_path) as trajectory:
+            for step, frame in enumerate(trajectory):
+                frame_data = trajectory.get_data()
+                if step == 0:
+                    box = np.array([frame_data['box'][i][i] for i in range(0, 3)])
+                if three_atom_plane:
+                    data_atom1 = frame_data['x'][mask_atom1]
+                    data_atom2 = frame_data['x'][mask_atom2]
+                    data_atom3 = frame_data['x'][mask_atom3]
+                    vec1 = data_atom1 - data_atom2
+                    vec2 = data_atom1 - data_atom3
+                    vec = np.cross(vec1, vec2, axis=1)
+                    vec_z = np.tile(np.array([0.0, 0.0, 1.0]), len(vec)).reshape(vec.shape)
+                    cos = np.sum(vec * vec_z, axis=1) / np.linalg.norm(vec, axis=1) / np.linalg.norm(vec_z, axis=1)
+                    traj_data.append(data_atom1)
+                    cos_data.append(cos)
+                else:
+                    raise ValueError("Another options are not ready yet")
+        cos_data = np.array(cos_data)
+        traj_data = np.array(traj_data)
+        bins = (np.array([6.273, 4.26]) // dx).astype(int)
+        steps = traj_data.shape[0]
+        traj_data = traj_data.reshape(-1, 3)
+        cos_data = cos_data.reshape(-1, )
+        hist_z, bins_z = np.histogram(traj_data[:, 2], bins=bins[0], range=[0.001, 6.272],
+                                      weights=cos_data)
+        volume_z = box[0] * box[1] * (bins_z[1] - bins_z[0])
+        hist_z = hist_z / volume_z / steps
+        return hist_z, bins_z
